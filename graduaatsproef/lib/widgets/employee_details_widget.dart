906c198b61b6de712c865ea7bdc22c77ae06bd26
Stef-w98 <stef.wouters18@gmail.com>
2023-05-23 17:11:58 +0200

    Fixed user table horizontal scroll

--- graduaatsproef/lib/widgets/employee_details_widget.dart
+++ graduaatsproef/lib/widgets/employee_details_widget.dart
@@ -30,6 +30,179 @@ class _EmployeeDetailsState extends State<EmployeeDetails> {
 
   @override
   Widget build(BuildContext context) {
+    /*final userAttendances = widget.attendances.where((attendance) {
+      if (startDate == null || endDate == null) {
+        return attendance.userId == widget.user.id;
+      }
+
+      DateTime checkInTime = attendance.checkInTime;
+      return attendance.userId == widget.user.id &&
+          checkInTime.isAfter(startDate!) &&
+          checkInTime.isBefore(endDate!);
+    }).toList();*/
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF1A1F24),
+      appBar: AppBar(
+        title: Text('Employee Details'),
+        backgroundColor: const Color(0xFF090F13),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth >= 600) {
+            // For larger screens (PC)
+            return buildPCLayout();
+          } else {
+            // For smaller screens (Phones)
+            return buildPhoneLayout();
+          }
+        },
+      ),
+    );
+  }
+
+  Widget buildPCLayout() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Flexible(
+          child: UserContactInfoWidget(userId: widget.user.id.toString()),
+        ),
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              DateRangePicker(
+                onDateRangeChanged: (DateTimeRange? newDateRange) {
+                  setState(() {
+                    startDate = newDateRange?.start;
+                    endDate = newDateRange?.end;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              Expanded(
+                child: AttendanceDataTableWidget(
+                  userId: widget.user.id,
+                  startDate: startDate,
+                  endDate: endDate,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: ElevatedButton.icon(
+            onPressed: () async {
+              Map<String, dynamic> result = await Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UpdateAccountScreen(user: widget.user),
+                ),
+              );
+              if (result != null && result['refresh'] == true) {
+                setState(() {
+                  widget.user = Users.fromMap(result['user']);
+                  startDate = null;
+                  endDate = null;
+                });
+              }
+            },
+            icon: Icon(Icons.edit),
+            label: Text('Update User'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildPhoneLayout() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 16),
+          DateRangePicker(
+            onDateRangeChanged: (DateTimeRange? newDateRange) {
+              setState(() {
+                startDate = newDateRange?.start;
+                endDate = newDateRange?.end;
+              });
+            },
+          ),
+          SizedBox(height: 16),
+          AttendanceDataTableWidget(
+            userId: widget.user.id,
+            startDate: startDate,
+            endDate: endDate,
+          ),
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton.icon(
+              onPressed: () async {
+                Map<String, dynamic> result = await Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        UpdateAccountScreen(user: widget.user),
+                  ),
+                );
+                if (result != null && result['refresh'] == true) {
+                  setState(() {
+                    widget.user = Users.fromMap(result['user']);
+                    startDate = null;
+                    endDate = null;
+                  });
+                }
+              },
+              icon: Icon(Icons.edit),
+              label: Text('Update User'),
+            ),
+          ),
+          SizedBox(height: 16),
+          Center(
+            child: UserContactInfoWidget(userId: widget.user.id.toString()),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+import 'package:graduaatsproef/models/users_model.dart';
+import 'package:graduaatsproef/models/nfc_cards_model.dart';
+import 'package:graduaatsproef/models/attendance_model.dart';
+import 'package:graduaatsproef/screens/update_user_screen.dart';
+import 'package:graduaatsproef/widgets/date_range_picker_widget.dart';
+import 'package:graduaatsproef/widgets/user_attendance_widget.dart';
+import 'package:graduaatsproef/widgets/user_contact_info_widget.dart';
+import 'package:intl/intl.dart';
+
+class EmployeeDetails extends StatefulWidget {
+  late final Users user;
+  final List<NfcCards> cards;
+  final List<Attendance> attendances;
+
+  EmployeeDetails({
+    required this.user,
+    required this.cards,
+    required this.attendances,
+  });
+
+  @override
+  _EmployeeDetailsState createState() => _EmployeeDetailsState();
+}
+
+class _EmployeeDetailsState extends State<EmployeeDetails> {
+  DateTime? startDate;
+  DateTime? endDate;
+  final dateFormatter = DateFormat('yyyy-MM-dd HH:mm');
+
+  @override
+  Widget build(BuildContext context) {
     final userAttendances = widget.attendances.where((attendance) {
       if (startDate == null || endDate == null) {
         return attendance.userId == widget.user.id;
@@ -90,103 +263,6 @@ class _EmployeeDetailsState extends State<EmployeeDetails> {
                   widget.user = Users.fromMap(result['user']);
                   startDate = null;
                   endDate = null;
-                });
-              }
-            },
-            child: Text('Update User'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-/*import 'package:flutter/material.dart';
-import 'package:graduaatsproef/models/users_model.dart';
-import 'package:graduaatsproef/models/nfc_cards_model.dart';
-import 'package:graduaatsproef/models/attendance_model.dart';
-import 'package:graduaatsproef/screens/update_user_screen.dart';
-import 'package:graduaatsproef/services/database/database_service.dart';
-import 'package:graduaatsproef/widgets/date_range_picker_widget.dart';
-import 'package:graduaatsproef/widgets/user_attendance_widget.dart';
-import 'package:graduaatsproef/widgets/user_contact_info_widget.dart';
-import 'package:intl/intl.dart';
-
-class EmployeeDetails extends StatefulWidget {
-  late final Users user;
-  final List<NfcCards> cards;
-  final List<Attendance> attendances;
-
-  EmployeeDetails({
-    required this.user,
-    required this.cards,
-    required this.attendances,
-  });
-
-  @override
-  _EmployeeDetailsState createState() => _EmployeeDetailsState();
-}
-
-class _EmployeeDetailsState extends State<EmployeeDetails> {
-  DateTimeRange? dateRange;
-  final dateFormatter = DateFormat('yyyy-MM-dd HH:mm');
-
-  @override
-  Widget build(BuildContext context) {
-    final userAttendances = widget.attendances.where((attendance) {
-      if (dateRange == null) {
-        return attendance.userId == widget.user.id;
-      }
-
-      DateTime checkInTime = attendance.checkInTime;
-      return attendance.userId == widget.user.id &&
-          checkInTime.isAfter(dateRange!.start) &&
-          checkInTime.isBefore(dateRange!.end);
-    }).toList();
-
-    return Scaffold(
-      backgroundColor: const Color(0xFF1A1F24),
-      appBar: AppBar(
-        title: Text('Employee Details'),
-        backgroundColor: const Color(0xFF090F13),
-      ),
-      body: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Flexible(
-            child: UserContactInfoWidget(userId: widget.user.id.toString()),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                DateRangePicker(
-                  onDateRangeChanged: (DateTimeRange? newDateRange) {
-                    setState(() {
-                      dateRange = newDateRange;
-                    });
-                  },
-                ),
-                SizedBox(height: 16),
-                Expanded(
-                  child: AttendanceDataTableWidget(userId: widget.user.id),
-                ),
-              ],
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              Map<String, dynamic> result = await Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => UpdateAccountScreen(user: widget.user),
-                ),
-              );
-              if (result != null && result['refresh'] == true) {
-                setState(() {
-                  // Update the user data using the result
-                  widget.user = Users.fromMap(result['user']);
-                  dateRange = null;
                 });
               }
             },
--- graduaatsproef/lib/widgets/employees_table_widget.dart
+++ graduaatsproef/lib/widgets/employees_table_widget.dart
@@ -11,8 +11,11 @@ class EmployeesTable extends StatefulWidget {
   final List<NfcCards> cards;
   final List<Attendance> attendance;
 
-  EmployeesTable(
-      {required this.users, required this.cards, required this.attendance});
+  EmployeesTable({
+    required this.users,
+    required this.cards,
+    required this.attendance,
+  });
 
   @override
   _EmployeesTableState createState() => _EmployeesTableState();
@@ -29,64 +32,69 @@ class _EmployeesTableState extends State<EmployeesTable> {
     print('hash long: $hashfull');
     print('hash short: $hash');
 
-    return DataTable(
-      headingTextStyle:
-          TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-      dataTextStyle: TextStyle(color: Colors.white),
-      columns: const [
-        DataColumn(label: Text('User ID')),
-        DataColumn(label: Text('Name')),
-        DataColumn(label: Text('Email')),
-        DataColumn(label: Text('Card UID')),
-      ],
-      rows: widget.users.asMap().entries.map((entry) {
-        int index = entry.key;
-        Users user = entry.value;
-        final card = widget.cards.firstWhere((card) => card.user_Id == user.id,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: DataTable(
+        headingTextStyle:
+            TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        dataTextStyle: TextStyle(color: Colors.white),
+        columns: const [
+          DataColumn(label: Text('User ID')),
+          DataColumn(label: Text('Name')),
+          DataColumn(label: Text('Email')),
+          DataColumn(label: Text('Card UID')),
+        ],
+        rows: widget.users.asMap().entries.map((entry) {
+          int index = entry.key;
+          Users user = entry.value;
+          final card = widget.cards.firstWhere(
+            (card) => card.user_Id == user.id,
             orElse: () => NfcCards(
-                  card_id: 1,
-                  user_Id: 1,
-                  card_Uid: '-',
-                  encryption_Key: '',
-                  createdAt: DateTime.now(),
-                  updatedAt: DateTime.now(),
-                ));
-        final cardUid = card.card_Uid;
+              card_id: 1,
+              user_Id: 1,
+              card_Uid: '-',
+              encryption_Key: '',
+              createdAt: DateTime.now(),
+              updatedAt: DateTime.now(),
+            ),
+          );
+          final cardUid = card.card_Uid;
 
-        return DataRow(
-          cells: [
-            DataCell(Text('${user.id}')),
-            DataCell(Text('${user.firstName} ${user.lastName}')),
-            DataCell(Text('${user.email}')),
-            DataCell(Text('$cardUid')),
-          ],
-          color: MaterialStateColor.resolveWith((Set<MaterialState> states) {
-            if (index == _hoverIndex) {
-              return Theme.of(context).colorScheme.primary.withOpacity(0.1);
-            }
-            return Colors.black26;
-          }),
-        ).withInkWell(
-          context,
-          onRowHover: (isHovered) {
-            setState(() {
-              _hoverIndex = isHovered ? index : null;
-            });
-          },
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => EmployeeDetails(
-                  user: user,
-                  cards: widget.cards,
-                  attendances: widget.attendance,
+          return DataRow(
+            cells: [
+              DataCell(Text('${user.id}')),
+              DataCell(Text('${user.firstName} ${user.lastName}')),
+              DataCell(Text('${user.email}')),
+              DataCell(Text('$cardUid')),
+            ],
+            color: MaterialStateColor.resolveWith((Set<MaterialState> states) {
+              if (index == _hoverIndex) {
+                return Theme.of(context).colorScheme.primary.withOpacity(0.1);
+              }
+              return Colors.black26;
+            }),
+          ).withInkWell(
+            context,
+            onRowHover: (isHovered) {
+              setState(() {
+                _hoverIndex = isHovered ? index : null;
+              });
+            },
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => EmployeeDetails(
+                    user: user,
+                    cards: widget.cards,
+                    attendances: widget.attendance,
+                  ),
                 ),
-              ),
-            );
-          },
-        );
-      }).toList(),
+              );
+            },
+          );
+        }).toList(),
+      ),
     );
   }
 }
